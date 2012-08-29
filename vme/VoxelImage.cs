ce2555f282c2186de87b12c18424b86453183441
Shahdee <13thshadow@gmail.com>
2012-08-29 20:17:44 +0400

    code refactoring2

--- vme/VoxelImage.cs
+++ vme/VoxelImage.cs
@@ -35,7 +35,7 @@ namespace vme
         private Float4 boxMinCon = new Float4(0, 0, 0, 0);
         private Float4 boxMaxCon = new Float4(512, 512, 512, 0);
         private float camfactorX;
-        //private float camfactorY;
+        private float camfactorY;
         private float camfactorZ;
         private OpenCLManager manager;
         private OpenCLNet.Program program;
@@ -51,7 +51,6 @@ namespace vme
         private Stopwatch clock;
         private ulong voxelctr, realctr;
 
-        
         public VoxelImage()
         {
             InitializeComponent();
@@ -66,25 +65,18 @@ namespace vme
             this.lx.Text = Convert.ToString(light.S0);
             this.ly.Text = Convert.ToString(light.S1);
             this.lz.Text = Convert.ToString(light.S2);
-
-
             this.trackminX.SetRange(0, 512);
             this.trackminY.SetRange(0, 512);
             this.trackminZ.SetRange(0, 512);
             this.trackmaxX.SetRange(0, 512);
             this.trackmaxY.SetRange(0, 512);
             this.trackmaxZ.SetRange(0, 512);
-
-
             this.trackminX.Value = (int)(boxMinCon.S0);
             this.trackminY.Value = (int)(boxMinCon.S1);
             this.trackminZ.Value = (int)(boxMinCon.S2);
-
             this.trackmaxX.Value = (int)(boxMaxCon.S0);
             this.trackmaxY.Value = (int)(boxMaxCon.S1);
             this.trackmaxZ.Value = (int)(boxMaxCon.S2);
-                        
-
             this.minX.Text = Convert.ToString(boxMinCon.S0);
             this.minY.Text = Convert.ToString(boxMinCon.S1);
             this.minZ.Text = Convert.ToString(boxMinCon.S2);
@@ -94,45 +86,37 @@ namespace vme
             this.kamb.Text = Convert.ToString(0.1);
             this.kdiff.Text = Convert.ToString(0.5);
             this.kspec.Text = Convert.ToString(0.4);
-
             this.tracklx.SetRange(-1000,1000);
             this.trackly.SetRange(-1000, 1000);
             this.tracklz.SetRange(-1000, 1000);
-
             this.tracklx.Value = (int)light.S0;
             this.trackly.Value = (int)light.S1;
             this.tracklz.Value = (int)light.S2;
-
             this.kc.Text = "1,0";
             this.kl.Text = "0,0";
             this.kq.Text = "0,0";
-
             this.specexp.Text = "1,0";
-
             outputBuffer = null;
             color_buffer =null;
             opacity = null;
-
             clock = new Stopwatch();
         }
 
-        public void  Draw()
-        {
+        public void  Draw(){
             // заблокировать поверхность
             BitmapData bd = output.LockBits(new Rectangle(0, 0, output.Width, output.Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
             DoRayCasting(bd);
             output.UnlockBits(bd);
         }
 
+        #region DICOMLoading
 
         public void LoadHeadDICOMTestDataSet(List<ushort> buffer, ushort num_of_images, double winCentre, double winWidth, double intercept, bool signed) 
         {
             ushort i = 0;
             short k;
-
             winCentre_vox = (int)(winCentre);
             winWidth_vox = (int)(winWidth);
-
             int winMax = Convert.ToInt32(winCentre_vox + 0.5 * winWidth);
             int winMin = winMax - (int)(winWidth);
             winMax -= 32768;
@@ -140,14 +124,10 @@ namespace vme
             vol = new VoxelVolume(517, manager);
             boxMinCon = new Float4(0, 0, 0, 0);
             boxMaxCon = new Float4(512, 512, 512, 0);
-
             /* для ускорения работы с буфером вокселей, ось x самая внутренняя. VolumetricMethodsInVisualEffects2010 */
-            for (var f = 1; f <= num_of_images; f++)
-            {
-                for (var y = 0; y < 512; y++)
-                {
-                    for (var x = 0; x < 512; x++)
-                    {
+            for (var f = 1; f <= num_of_images; f++){
+                for (var y = 0; y < 512; y++){
+                    for (var x = 0; x < 512; x++){
                         i = buffer[(f - 1) * 512 * 512 + (x * 512 + y)];
                         k = (short)(i - 32768);
                         if (k > winMin && k < winMax)  // учесть преобразование wincentre
@@ -160,13 +140,12 @@ namespace vme
                             vol.SetValue(xx, (330 - zz * 2 + 2), yy, k);
                         }
                     }
-
                 }
                 GC.Collect();
             }
             GC.Collect();
             camfactorX = 2;
-            //camfactorY = 2;
+            camfactorY = 2;
             camfactorZ = 2;
         }
 
@@ -174,10 +153,8 @@ namespace vme
         {
             ushort i = 0;
             short k;
-
             winCentre_vox = (int)(winCentre);
             winWidth_vox = (int)(winWidth);
-
             int winMax = Convert.ToInt32(winCentre_vox + 0.5 * winWidth);
             int winMin = winMax - (int)(winWidth);
             winMax -= 32768;
@@ -185,7 +162,6 @@ namespace vme
             vol = new VoxelVolume(517, manager);
             boxMinCon = new Float4(0, 0, 0, 0);
             boxMaxCon = new Float4(512, 512, 512, 0);
-
             /* для ускорения работы с буфером вокселей, ось x самая внутренняя. VolumetricMethodsInVisualEffects2010 */
             for (var f = 1; f <= num_of_images; f++)
             {
@@ -200,9 +176,12 @@ namespace vme
                             var xx = (int)((x / 512.0) * (513.0 - 5));
                             var yy = 512 - (int)((y / 512.0) * (513.0 - 5));
                             var zz = (int)((f / 167.0) * (166.0));
-                            vol.SetValue(xx, (330 - zz * 2), yy, k);
-                            vol.SetValue(xx, (330 - zz * 2 + 1), yy, k);
-                            vol.SetValue(xx, (330 - zz * 2 + 2), yy, k);
+                            //vol.SetValue(xx, (330 - zz * 2), yy, k);
+                            //vol.SetValue(xx, (330 - zz * 2 + 1), yy, k);
+                            //vol.SetValue(xx, (330 - zz * 2 + 2), yy, k);
+                            vol.SetValue(xx, yy,(330 - zz * 2), k);
+                            vol.SetValue(xx, yy,(330 - zz * 2 + 1), k);
+                            vol.SetValue(xx, yy,(330 - zz * 2 + 2), k);
                         }
                     }
 
@@ -211,14 +190,15 @@ namespace vme
             }
             GC.Collect();
             camfactorX = 2;
-            //camfactorY = 2;
+            camfactorY = 2;
             camfactorZ = 2;
         }
+        #endregion
 
+        #region GetRegion
 
         private unsafe Mem GetColors()
         {
-
             fixed (uint* dataptr = form_this.colors)
             {
                 color_buffer = manager.Context.CreateBuffer(MemFlags.COPY_HOST_PTR, form_this.colors.Count() * 4, new IntPtr(dataptr));
@@ -246,19 +226,14 @@ namespace vme
             return counter;
         }
 
-        private unsafe void DoRayCasting(BitmapData output)
-        {
-           
-            try
-            {
+        #endregion
+
+        private unsafe void DoRayCasting(BitmapData output){
+            try{
                 int deviceIndex = 0;
                 outputBuffer = manager.Context.CreateBuffer(MemFlags.USE_HOST_PTR, output.Stride * output.Height, output.Scan0);
-
-                
-
                 if (first || changeDistance)
                 {
-
                     // модель камеры UVN 
                     camPos = new Float4()
                     {
@@ -273,88 +248,64 @@ namespace vme
                     changeDistance = false;
                     camPosOld = camPos;
                 }
-
-                else
-                {
+                else{
                     // поворот вокруг оси куба визуализации
-                    if (angleChange && leftChange)
-                    {
+                    if (angleChange && leftChange){
                         camPosOld.S0 -= camLookAt.S0;
                         camPosOld.S2 -= camLookAt.S2;
-
                         camPos.S0 = (float)Math.Cos(camAngle * Math.PI / 180) * camPosOld.S0 + (float)Math.Sin(camAngle * Math.PI / 180) * camPosOld.S2;
                         camPos.S1 = vol.GetSize() / 2;
                         camPos.S2 = -(float)Math.Sin(camAngle * Math.PI / 180) * camPosOld.S0 + (float)Math.Cos(camAngle * Math.PI / 180) * camPosOld.S2;
                         camPos.S3 = 0;
-
                         camPos.S0 += camLookAt.S0;
                         camPos.S2 += camLookAt.S2;
-
                         camPosOld = camPos;
                         angleChange = false;
                         leftChange = false;
                     }
-                    if (angleChange && rightChange)
-                    {
+                    if (angleChange && rightChange){
                         camPosOld.S0 -= camLookAt.S0;
                         camPosOld.S2 -= camLookAt.S2;
-
                         camPos.S0 = (float)Math.Cos(camAngle * Math.PI / 180) * camPosOld.S0 - (float)Math.Sin(camAngle * Math.PI / 180) * camPosOld.S2;
                         camPos.S1 = vol.GetSize() / 2;
                         camPos.S2 = (float)Math.Sin(camAngle * Math.PI / 180) * camPosOld.S0 + (float)Math.Cos(camAngle * Math.PI / 180) * camPosOld.S2;
                         camPos.S3 = 0;
-
                         camPos.S0 += camLookAt.S0;
                         camPos.S2 += camLookAt.S2;
-
                         camPosOld = camPos;
                         angleChange = false;
                         leftChange = false;
-
                     }
-
                 }
 
-                camLookAt = new Float4()
-                {
+                camLookAt = new Float4(){
                     S0 = vol.GetSize() / camfactorX,
                     S1 = vol.GetSize() / camfactorX,
                     S2 = vol.GetSize() / camfactorZ,
                     S3 = 0
                 };
-
-                //light = camPos;
                 
                 // направление камеры, UVN модель
                 camForward = camLookAt.Sub(camPos).Normalize(); // направление просмотра
                 var up = new Float4(0.0f, 1.0f, 0.0f, 0.0f);
                 var right = MathClass.Cross(up, camForward).Normalize().Times(1.5f);
                 up = MathClass.Cross(camForward, right).Normalize().Times(-1.5f);
-
-
-
                 /*  обработка выходного изображения BitmapData в OpenCl устройстве */
-                for (var x = 0; x < output.Width; x += blocksize)
-                {
-                    for (var y = 0; y < output.Height; y += blocksize)
-                    {
+                for (var x = 0; x < output.Width; x += blocksize){
+                    for (var y = 0; y < output.Height; y += blocksize){
                         var rayTracingGlobalWorkSize = new IntPtr[2]; // work_dim = 2
                         rayTracingGlobalWorkSize[0] = (IntPtr)(output.Width - x > blocksize ? blocksize : output.Width - x);
                         rayTracingGlobalWorkSize[1] = (IntPtr)(output.Height - y > blocksize ? blocksize : output.Height - y);
-
                         var rayTracingGlobalOffset = new IntPtr[2];
                         rayTracingGlobalOffset[0] = (IntPtr)x; 
                         rayTracingGlobalOffset[1] = (IntPtr)y; 
-
                         float ka = (float)(Convert.ToDouble(kamb.Text));
                         float kd = (float)(Convert.ToDouble(kdiff.Text));
                         float ks = (float)(Convert.ToDouble(kspec.Text));
                         float exp = (float)(Convert.ToDouble(specexp.Text));
-
                         float kkc = (float)(Convert.ToDouble(this.kc.Text));
                         float kkl = (float)(Convert.ToDouble(this.kl.Text));
                         float kkq = (float)(Convert.ToDouble(this.kq.Text));
-
                         /* передали аргументы в kernel функцию */
                         kernel.SetArg(0, output.Width);
                         kernel.SetArg(1, output.Height);
@@ -389,29 +340,22 @@ namespace vme
                         kernel.SetArg(30, kkl);
                         kernel.SetArg(31, kkq);
                         kernel.SetArg(32, GetCounter());
-                  
                         /* Ставит в очередь команду для исполнения kernel на устройстве */
-                        /*
-                            rayTracingGlobalOffset - 
+                        /*  rayTracingGlobalOffset - 
                          *  globalWorkOffset: может использоваться для указания массива значений
                          *  размерности work_dim unsigned который описывает смещение используемое для расчета  global ID  work-item
                          *  вместо того чтобы global IDs всегда начинался со смещение (0, 0,... 0).
-                            
                          *  rayTracingGlobalWorkSize -
-                         *  globalWorkSize: общее число global work-items вычисляется как global_work_size[0] *...* global_work_size[work_dim - 1].
-                         *  
-                         */
+                         *  globalWorkSize: общее число global work-items вычисляется как global_work_size[0] *...* global_work_size[work_dim - 1].*/
                         manager.CQ[deviceIndex].EnqueueNDRangeKernel(kernel, 2, rayTracingGlobalOffset, rayTracingGlobalWorkSize, null);
                     }
                 }
 
                 /* подождали пока все work-items выполнятся */
                 manager.CQ[deviceIndex].EnqueueBarrier();
-
                 /* для того чтобы получить доступ к памяти и записать в выходное изображение мы просим у OpenCL *наложить* данные в хост-устройство */
                 IntPtr p = manager.CQ[deviceIndex].EnqueueMapBuffer(outputBuffer, true, MapFlags.READ, IntPtr.Zero, (IntPtr)(output.Stride * output.Height));
                 IntPtr z = manager.CQ[deviceIndex].EnqueueMapBuffer(counter, true, MapFlags.READ_WRITE, IntPtr.Zero, (IntPtr)(sizeof(ulong)));
-            
                 /* когда мы заканчиваем работать с буфером надо вызвать эту функцию */
                 manager.CQ[deviceIndex].EnqueueUnmapMemObject(outputBuffer, p);
                 manager.CQ[deviceIndex].EnqueueUnmapMemObject(counter, z);
@@ -419,65 +363,48 @@ namespace vme
                 realctr += voxelctr;
                 voxelCounter.Text = Convert.ToString(realctr);
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex){
                 MessageBox.Show("Ray casting exception:" + ex.Message, "Exception");
                 Environment.Exit(-1);
             }
-            finally
-            {
-                if (outputBuffer != null)
-                {
+            finally{
+                if (outputBuffer != null){
                     outputBuffer.Dispose();
-
                 }
             }
-
         }
 
-
-        internal void Idle()
-        {
-
-            if (initialize)
-            {
+        internal void Idle(){
+            if (initialize){
                 Draw();
                 VSurface.Refresh();
             }
         }
 
-        public void VoxelImage_Load(List<ushort> buffer, ushort num_of_images, double winCentre, double winWidth, double intercept, bool signed, Main form)
-        {
-            try
-            {
+        public void VoxelImage_Load(List<ushort> buffer, ushort num_of_images, double winCentre, double winWidth, double intercept, bool signed, Main form){
+            try{
                 InitializeOpenCL();
                 LoadDICOMTestDataSet(buffer, num_of_images, winCentre, winWidth, intercept, signed);
                 initialize = true;
                 form_this = form;
 
             }
-            catch (Exception ex)
-            {
+            catch (Exception ex){
                 MessageBox.Show(ex.Message, " Ошибка инициализации OpenCl  или проблема с загрузкой DICOM файлов!");
             }
         }
 
-        private void InitializeOpenCL()
-        {
-            if (OpenCL.NumberOfPlatforms == 0)
-            {
+        private void InitializeOpenCL(){
+            if (OpenCL.NumberOfPlatforms == 0){
                 MessageBox.Show("OpenCL не поддерживается вашей системой!");
                 Application.Exit();
             }
-
             manager = new OpenCLManager();
             manager.AttemptUseBinaries = true;
             manager.AttemptUseSource = true;
             manager.RequireImageSupport = false;
             manager.BuildOptions = "";
-
             manager.CreateDefaultContext(0, DeviceType.ALL);
-
             // Компиляция OpenCL кода
             program = manager.CompileSource(Properties.Resources.DVR);
             kernel = program.CreateKernel("DVR");
@@ -671,8 +598,7 @@ namespace vme
     #endregion
 
 
-        private void VoxelImage_FormClosing(object sender, EventArgs e)
-        {
+        private void VoxelImage_FormClosing(object sender, EventArgs e){
             vol.ReturnBuffer().Dispose();
             color_buffer.Dispose();
             opacity.Dispose();
@@ -683,7 +609,6 @@ namespace vme
             manager.Dispose();
             VSurface.Dispose();
             GC.Collect();
-
         }
 
         #region TrackBars
@@ -757,8 +682,7 @@ namespace vme
 
         #endregion
 
-        private void tracklx_Scroll(object sender, EventArgs e)
-        {
+        private void tracklx_Scroll(object sender, EventArgs e){
             light.S0 = (float)tracklx.Value;
             lx.Text = Convert.ToString(tracklx.Value);
             if (initialize)
@@ -768,8 +692,7 @@ namespace vme
             }
         }
 
-        private void trackly_Scroll(object sender, EventArgs e)
-        {
+        private void trackly_Scroll(object sender, EventArgs e){
             light.S1 = (float)trackly.Value;
             ly.Text = Convert.ToString(trackly.Value);
             if (initialize)
@@ -779,8 +702,7 @@ namespace vme
             }
         }
 
-        private void tracklz_Scroll(object sender, EventArgs e)
-        {
+        private void tracklz_Scroll(object sender, EventArgs e){
             light.S2 = (float)tracklz.Value;
             lz.Text = Convert.ToString(tracklz.Value);
             if (initialize)
@@ -789,7 +711,5 @@ namespace vme
                 VSurface.Refresh();
             }
         }
-
     }
-        
 }
